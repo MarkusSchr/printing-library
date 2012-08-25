6a5d8f7c9a53ac84d91ca607af0b5da3902d86ed
aicrosoft1104 <aicrosoft1104@gmail.com>
2012-08-25 14:47:23 +0000

    Add evaluate pages function to multitablesunit

--- branches/Printing/MultiTablesUnit.h
+++ branches/Printing/MultiTablesUnit.h
@@ -22,6 +22,7 @@ namespace Printing
 		// printing
 		virtual int PreviewUnit(CDC* pOriginDC, BOOL bGetPageOnly, int from, int to);
 		virtual int Paint(int from, int to);
+		virtual int EveluateUnitPages(CDC* pOriginDC, int from, int to);
 
 	private:
 		virtual void CreatePrintFonts();
